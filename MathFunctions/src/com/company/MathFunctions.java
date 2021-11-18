9721a33f0050fd61cf5db3d0ae2e57ec1c046f81
misquamacus666 <94652205+misquamacus666@users.noreply.github.com>
2021-11-19 00:26:13 +0100

    new

---
+++ MathFunctions/src/com/company/MathFunctions.java
@@ -0,0 +1,15 @@
+import static java.lang.Math.*;
+
+public class MathFunctions {
+
+    public static void main(String[] args) {
+        double first = 9.0;
+        int second = 4;
+        double sqrt = sqrt(first);
+        double power = pow(first, second);
+
+        System.out.println(first + sqrt);
+        System.out.println(first + second + power);
+
+    }
+}
---
+++ Operators/src/com/company/Operators.java
@@ -0,0 +1,18 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.*;
+public class Operators {
+
+    public static void main(String[] args) {
+        double first = 9.0;
+        int second = 4;
+
+        double sqrt = Math.sqrt(first);
+        double power = Math.pow(first, second);
+
+        System.out.println("Pierwiastek z " + first + "wynosi: ");
+        System.out.println("Liczba: " + first + " do potęgi: " + second + " to " + power);
+    }
+
+    }
+
